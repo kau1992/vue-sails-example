b385d652de160dd84b7838cd09453dafd473f6f3
Julian Claus <kontakt@julian-claus.de>
2017-07-24 20:35:59 +0200

    fixed code style

--- backend/test/functional/specs/Product.spec.js
+++ backend/test/functional/specs/Product.spec.js
@@ -9,7 +9,6 @@ describe('Product', function () {
   describe('get', function () {
     it('should return a non empty JSON with the product', function (done) {
       sails.services.cryptographyservice.decrypt = () => 1
-
       request(sails.hooks.http.app)
         .get('/api/user/product/get')
         .query({
