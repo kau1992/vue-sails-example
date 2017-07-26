3ddcee8de5bd5a9f3da41ea0ce233be0fb736c3c
cetaju <kontakt@julian-claus.de>
2017-07-26 18:42:27 +0200

    code style

--- backend/api/policies/isAuthorized.js
+++ backend/api/policies/isAuthorized.js
@@ -1,16 +1,18 @@
-module.exports = function isAuthorized (req, res, next) {
-  let token = req.headers.token
+module.exports = {
+  isAuthorized (req, res, next) {
+    let token = req.headers.token
 
-  if ('undefined' === token) return res.forbidden()
+    if ('undefined' === token) return res.forbidden()
 
-  let decryptedSessionStorageToken = TokenService.verify(token)
+    let decryptedSessionStorageToken = TokenService.verify(token)
 
-  User
-    .findOne({
-      id: decryptedSessionStorageToken.id
-    })
-    .exec((error, user) => {
-      if (error) return res.serverError(error)
-      if (user) return next()
-    })
+    User
+      .findOne({
+        id: decryptedSessionStorageToken.id
+      })
+      .exec((error, user) => {
+        if (error) return res.serverError(error)
+        if (user) return next()
+      })
+  }
 }
