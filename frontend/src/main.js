f0c259357e43e69c6fd279f19d93238dcac11a0d
cetaju <kontakt@julian-claus.de>
2017-08-03 21:17:41 +0200

    improved browser width check, lazy load application dependencies

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,3 +1,13 @@
-if (window.innerWidth >= 768) {
-  require('./bootstrap.desktop')
-} else require('./bootstrap.mobile')
+/**
+* @see {@link https://stackoverflow.com/questions/1038727/how-to-get-browser-width-using-javascript-code#1038781}
+*/
+const browserWidth = Math.max(
+  document.body.scrollWidth,
+  document.documentElement.scrollWidth,
+  document.body.offsetWidth,
+  document.documentElement.offsetWidth,
+  document.documentElement.clientWidth
+)
+
+if (browserWidth >= 768) import('./bootstrap.desktop')
+if (browserWidth < 768) import('./bootstrap.mobile')
