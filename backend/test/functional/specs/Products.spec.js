0e44d39750b64e7b879dc1f6c4696d85974d6619
cetaju <kontakt@julian-claus.de>
2017-08-25 08:20:49 +0200

    testing find products by user

--- backend/test/functional/specs/Products.spec.js
+++ backend/test/functional/specs/Products.spec.js
@@ -20,4 +20,45 @@ describe('Products', function() {
       })
   })
 
+  it('should find products by given user', function(done) {
+    let name = 'Joe'
+    let password = 'toasty'
+    let product = {
+      title: 'Cell phone',
+      description: 'Now with a cool touch display and many colors.',
+      price: 199.95,
+      user: 1
+    }
+
+    request(sails.hooks.http.app)
+      .post('/api/user/products/product/post')
+      .set('Cookie', 'user=92f06e135b1749215c2a2bbc6261ffaa')
+      .send(product)
+      .expect(200)
+      .then(() => {
+
+        request(sails.hooks.http.app)
+          .post('/api/register/post')
+          .send({
+            name,
+            password
+          })
+          .expect(200)
+          .then(() => {
+
+            request(sails.hooks.http.app)
+              .get('/api/user/products/getProductsByUser')
+              .set('Cookie', 'user=92f06e135b1749215c2a2bbc6261ffaa')
+              .expect('Content-Type', /json/)
+              .expect(200)
+              .end((error, response) => {
+                if (error) return done(error)
+
+                chai.assert.isNotEmpty(response.body)
+                done()
+              })
+          })
+      })
+  })
+
 })
