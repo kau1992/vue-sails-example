cb79ba71d302689eefa691a3fa831733e415ebd5
cetaju <kontakt@julian-claus.de>
2017-10-27 09:52:14 +0200

    check for node environment to skip policy

--- backend/api/policies/isAuthorized.js
+++ backend/api/policies/isAuthorized.js
@@ -1,4 +1,5 @@
 module.exports = function isAuthorized (req, res, next) {
+  if (process.env.NODE_ENV === 'test') return next()
   let token = req.headers.token
 
   if ('undefined' === token) return res.forbidden()
