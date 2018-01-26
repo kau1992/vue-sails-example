13193eea5f85473e381abe01bd71ea26d772acf5
rymw3p <kontakt@julian-claus.de>
2018-01-26 21:28:17 +0100

    test (client): use cypress.io for tests

---
+++ frontend/cypress/fixtures/mocks.js
@@ -0,0 +1,28 @@
+import faker from 'faker'
+
+export default (entity, limit, parameters) => {
+  const entities = {
+    products: limit => {
+      let products = []
+      for (let index = 0; index < limit; index++) {
+        products.push({
+          id: index,
+          title: faker.commerce.productName(),
+          description: faker.lorem.sentence(),
+          price: faker.commerce.price(),
+          user: {
+            id: index,
+            name: faker.name.firstName()
+          }
+        })
+      }
+
+      return {
+        products,
+        amountOfProducts: parameters.amountOfProducts
+      }
+    }
+  }
+
+  return entities[entity](limit)
+}
